e293da34bf0c9c0cdcaaebc289f901c1f1917970
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-13 15:43:41 -0500

    setup Navbar and About

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from './components/Nav'
 import About from './components/About';
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
