9df617c93d8efddd16080b5be3a16cb8e9cde873
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-18 16:39:35 -0500

    error on test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import Nav from './components/Nav'
+import React, { useState } from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-
 
 function App() {
   const [categories] = useState([
@@ -25,10 +24,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,28 +1,16 @@
-import React from "react";
+import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
-
-
 
 function Gallery(props) {
-    const currentCategory = {
-        name: "commercial",
-        description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    };
-    return (
-        <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-            <div className= "flex-row">
-                <img
-                  src={photo}
-                  alt= "Commercial Example"
-                  className= "img-thumbanil mx-1"
-                />
-            </div>
-        </section>
-    );
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
 }
 
 export default Gallery;
