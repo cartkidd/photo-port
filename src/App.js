76b95be22d9f4e097a8cb76bf06191ae371ae276
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-18 21:30:04 -0500

    Contactform setup

--- src/App.js
+++ src/App.js
@@ -18,17 +18,25 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
+      {!contactSelected ? (
+          <>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
+        </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
